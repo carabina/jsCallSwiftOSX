84e4013bb4a486d8070aba87654b2364da0598ff
ducbao414 <md231@MacBook-Air.local>
2015-09-15 13:01:30 +0700

    Initial Commit

---
+++ jsCallSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  jsCallSwift
+//
+//  Created by bmbhh on 9/15/15.
+//  Copyright (c) 2015 bmbhh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
